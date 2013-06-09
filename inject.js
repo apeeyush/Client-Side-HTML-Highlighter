054c0f94c44a69fae78444529076bc8197782578
shubsin <shubhamsingh7135172@gmail.com>
2013-06-09 17:18:40 +0530

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,5 +1,30 @@
-var bodyTag = document.getElementsByTagName("body")[0];
-bodyTag.addEventLintener("onmouseup", selc, true);
+$(document).ready(function(){
+  $("body").mouseup(function(){
+    selc();
+  });
+});
+
 function selc() {
-    alert(window.getSelection);
+    var selction  = window.getSelection();
+    var text = selction.toString();
+    if(text.length != 0){
+        var range = selction.getRangeAt(0);
+        alert(makeXPath(range.startContainer));
+    };
+};
+
+function makeXPath (node, currentPath) {
+  /* this should suffice in HTML documents for selectable nodes, XML with namespaces needs more code */
+  currentPath = currentPath || '';
+  switch (node.nodeType) {
+    case 3:
+    case 4:
+      return makeXPath(node.parentNode, 'text()[' + (document.evaluate('preceding-sibling::text()', node, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength + 1) + ']');
+    case 1:
+      return makeXPath(node.parentNode, node.nodeName + '[' + (document.evaluate('preceding-sibling::' + node.nodeName, node, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength + 1) + ']' + (currentPath ? '/' + currentPath : ''));
+    case 9:
+      return '/' + currentPath;
+    default:
+      return '';
+  }
 };
