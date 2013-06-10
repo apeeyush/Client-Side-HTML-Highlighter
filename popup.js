e36098c1b7fef64a842deb3d94ffcc337f6d07a2
shubsin <shubhamsingh7135172@gmail.com>
2013-06-10 17:41:36 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,10 +7,6 @@ function code() {
         requestXPaths();
     });
 
-    $("img#recall").click(function () {
-        recall();
-    })
-
     chrome.tabs.getSelected(null, function (tab) {
         tabURL = tab.url;
         tabID = tab.id;
@@ -18,14 +14,34 @@ function code() {
 
     function requestXPaths() {
         chrome.tabs.sendMessage(tabID, { mssg: "giveXPath" }, function (response) {
-            alert(response.reply);
+            //alert(response.reply);
+            addToStorage(response.reply);
             //localStorage.setItem(tabURL, response.XPath);
         });
     };
 
-    function recall(){
-        alert(localStorage.getItem(tabURL));
-    }
+    function addToStorage(reply){
+        var oldDataString = localStorage.getItem(tabURL);
+        if(oldDataString == null){
+            oldDataString = '{"count":0}';
+            localStorage.setItem(tabURL, oldDataString);
+        };
+        var oldDataObj = JSON.parse(oldDataString);
+        var newDataString = "";
+        var newDataCount = oldDataObj.count + 1;
+        newDataString += '{"count":' + newDataCount;
+        var i  = 1;
+        for(key in oldDataObj){
+            if(typeof(oldDataObj[key]) != typeof(1)){
+                newDataString += ',' + '"sel' + i + '":' + JSON.stringify(oldDataObj[key]);
+                i += 1;
+            }
+        };
+        newDataString += ',' + '"sel' + newDataCount + '":' + JSON.stringify(reply) + '}';
+        alert(newDataString);
+        localStorage.setItem(tabURL, newDataString);
+
+    };
 };
 
    
