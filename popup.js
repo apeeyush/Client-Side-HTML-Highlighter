2e9fd747c5f08e297136d2ea371ca7e1f6c5c478
shubsin <shubhamsingh7135172@gmail.com>
2013-06-10 13:29:24 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,9 +3,13 @@ var tabURL,tabID;
 $(document).ready(code);
 
 function code() {
-    $("img").click(function () {
+    $("img#highlight").click(function () {
         sendMssg();
     });
+
+    $("img#recall").click(function () {
+        recall();
+    })
 
     chrome.tabs.getSelected(null, function (tab) {
         tabURL = tab.url;
@@ -15,8 +19,13 @@ function code() {
     function sendMssg() {
         chrome.tabs.sendMessage(tabID, { mssg: "giveXPath" }, function (response) {
             alert(response.XPath);
+            localStorage.setItem(tabURL, response.XPath);
         });
     };
+
+    function recall(){
+        alert(localStorage.getItem(tabURL));
+    }
 };
 
    
