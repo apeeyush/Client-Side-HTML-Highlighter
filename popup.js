4b751802cfc21b927f85476cdf151fe5c3490a7b
shubsin <shubhamsingh7135172@gmail.com>
2013-06-16 16:41:23 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,29 +1,27 @@
-var tabURL,tabID;
+var tabID;  //tabID is needed for sending tab 
 
-$(document).ready(code);
-
-function code() {
-    $("button#highlight").click(function () {
-        sendMssg("highlight");
-    });
-
-    $("button#clear").click(function () {
-        sendMssg("clear");
-    });
-
+$(document).ready(function() {
+    //getting id of current tab
     chrome.tabs.getSelected(null, function (tab) {
-        tabURL = tab.url;
-        if (tabURL.indexOf("#") != -1) {
-            tabURL = tabURL.substring(0, tabURL.indexOf("#"));
-        }
         tabID = tab.id;
     });
 
-    function sendMssg(message) {
+    //messaging user's action to injected script
+    $("#highlight").click(function () {
+        sendMessg("highlight");
+    });
+
+    $("#unhighlight").click(function () {
+        sendMessg("unhighlight");
+    });
+
+    $("#clear_all").click(function () {
+        sendMessg("clear_all");
+    });
+});
+
+ function sendMessg(message) {
         chrome.tabs.sendMessage(tabID, { mssg: message }, function (response) {
             
         });
-    };
-};
-
-   
+    };  
